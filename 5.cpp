03b1ab241f16453d7250e51b43f40945bc0fb28d
akshatjain1004 <jain.53@iitj.ac.in>
2022-05-11 02:02:19 +0530

    Day-2

--- 5.cpp
+++ 5.cpp
@@ -13,7 +13,15 @@ int main(int argc, char const *argv[])
         cin>>arr[i];
     }
 
-    sort(arr,arr+n);
+    int j=0;
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]<0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+    
     for (int i = 0; i < n; i++)
     {
         cout<<arr[i]<<" ";
