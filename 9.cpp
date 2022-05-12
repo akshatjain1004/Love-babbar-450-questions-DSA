cd4ef7cc987bc74d46848000970e9662b7e5d9de
akshatjain1004 <jain.53@iitj.ac.in>
2022-05-12 23:40:06 +0530

    Day-3

---
+++ 9.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int getMinDiff(int arr[], int n, int k) {
+        sort(arr,arr+n);
+        int ma= arr[n-1];
+        int mi= arr[0];
+        int res= ma-mi;
+        for(int i=1;i<n;i++){
+
+            ma= max(arr[i-1]+k,arr[n-1]-k);
+            mi= min(arr[i]-k,arr[0]+k);
+            res= min(ma-mi,res);
+            
+        }
+        return res;
+    }
+
+int main(int argc, char const *argv[])
+{
+    int k;
+    cin>>k;
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    cout<<getMinDiff(arr,n,k);
+    return 0;
+}
