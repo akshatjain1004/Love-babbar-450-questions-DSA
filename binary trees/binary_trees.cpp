c535d3999c9eddc4a881e396f6e414a8f6f9c4a9
akshatjain1004 <jain.53@iitj.ac.in>
2022-07-01 00:47:17 +0530

    Day-31

--- binary trees/binary_trees.cpp
+++ binary trees/binary_trees.cpp
@@ -14,6 +14,35 @@ class node{
     }
 };
 
+void preOrder(node* root){
+    cout<<root->data<<" ";
+    if(root->left!=NULL){
+        preOrder(root->left);
+    }
+    if(root->right!=NULL){
+        preOrder(root->right);
+    }
+}
+
+void inOrder(node* root){
+    
+    if(root==NULL){
+        return;
+    }
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+}
+
+void postOrder(node* root){
+    if(root==NULL){
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+}
+
 int main(int argc, char const *argv[])
 {
     node* root = new node(1);
@@ -21,11 +50,15 @@ int main(int argc, char const *argv[])
     root->right= new node(3);
     root->left->left= new node(4);
     root->left->right= new node(5);
-
+    root->right->left= new node(6);
+    root->right->right= new node(7);
     node* temp= root;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp= temp->left;
-    }
+    // while(temp!=NULL){
+    //     cout<<temp->data<<" ";
+    //     temp= temp->left;
+    // }
+    // preOrder(root);
+    // inOrder(root);
+    postOrder(root);
     return 0;
 }
